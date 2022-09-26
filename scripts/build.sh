3669456fe2c1c3f7eda643006703fcc3021b526d
yo <chocomate.dowob.y912@gmail.com>
2022-09-26 18:03:17 +0900

    add custom keyball39

---
+++ scripts/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR=$(pwd)
+BASEFILE=$(readlink -f $0)
+SCRIPT_DIR=$(dirname $BASEFILE)
+QMK_KEYBOARDS_KEYBALL_DIR="$(cd ${SCRIPT_DIR%/}/../qmk_firmware/keyboards/keyball; pwd)"
+
+cd $SCRIPT_DIR
+. ./.env.local
+
+
+DOCKER_CONTAINER_NAME='qmkfm/qmk_cli'
+
+cd $QMK_HOME
+docker build . -t $DOCKER_CONTAINER_NAME
+
+echo "Build: $BUILD_TARGET"
+docker run -it --rm \
+	-v $QMK_HOME:/qmk_firmware \
+	-v $QMK_KEYBOARDS_KEYBALL_DIR:/qmk_firmware/keyboards/keyball \
+        $DOCKER_CONTAINER_NAME \
+	/bin/bash -lc "cd /qmk_firmware && make -B $BUILD_TARGET"
