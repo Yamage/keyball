9708cc7d6d0b253bb72fae397820d09f98aff6e7
Yamage <chocomate.dowob.y912@gmail.com>
2022-11-22 00:11:07 +0900

    chore: modified scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,19 +9,27 @@ fi
 
 if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
     echo 'Usage: ./build.sh []
-TODO
+todo: add help
 '
     exit
 fi
 
 cd "$(dirname "$0")"
 
+TARGET_KEYBOARD="${1:-keyball/keyball39custom}"
+TARGET_KEYBOARD_FAMILY="${TARGET_KEYBOARD%/*}"
+TARGET_KEYBOARD_KEYMAP="${2:-via}"
+
 main() {
 	CURRENT_DIR=$(pwd)
 	BASEFILE=$(readlink -f $0)
 	SCRIPT_DIR=$(dirname $BASEFILE)
 	DIST_DIR=$(cd $SCRIPT_DIR && cd ../ && echo $PWD/dist)
-	QMK_KEYBOARDS_KEYBALL_DIR="$(cd ${SCRIPT_DIR%/}/../qmk_firmware/keyboards/keyball; pwd)"
+	QMK_KEYBOARDS_DIR="$(cd ${SCRIPT_DIR%/}/../qmk_firmware/keyboards/${TARGET_KEYBOARD_FAMILY}; pwd)"
+
+	echo "Keyboad: ${TARGET_KEYBOARD}"
+	echo "Keyboad Familly: ${TARGET_KEYBOARD_FAMILY}"
+	echo "Keyboad Keymap: ${TARGET_KEYBOARD_KEYMAP}"
 
 	cd $SCRIPT_DIR
 	. ./.env.local
@@ -32,10 +40,11 @@ main() {
 	cd $QMK_HOME
 	docker build . -t $DOCKER_CONTAINER_NAME
 
+	BUILD_TARGET=${TARGET_KEYBOARD}:${TARGET_KEYBOARD_KEYMAP}
 	echo "Build: $BUILD_TARGET"
 	docker run -it --rm \
 		-v $QMK_HOME:/qmk_firmware \
-		-v $QMK_KEYBOARDS_KEYBALL_DIR:/qmk_firmware/keyboards/keyball \
+		-v $QMK_KEYBOARDS_DIR:/qmk_firmware/keyboards/${TARGET_KEYBOARD_FAMILY} \
         $DOCKER_CONTAINER_NAME \
 		/bin/bash -lc "cd /qmk_firmware && make -B $BUILD_TARGET"
 
@@ -44,7 +53,8 @@ main() {
 	mkdir -p $DIST_DIR
 	BUILD_TARGET_TMP=${BUILD_TARGET//\//_}
 	BUILD_TARGET_TMP=${BUILD_TARGET_TMP//:/_}
-	mv -f ${BUILD_TARGET_TMP}.hex $DIST_DIR
+	mv -f ${BUILD_TARGET_TMP}.hex $DIST_DIR || true
+	mv -f ${BUILD_TARGET_TMP}.uf2 $DIST_DIR || true
 
 	echo "MOVE Firmware to Dist: ${DIST_DIR}"
 }
--- scripts/launch.sh
+++ scripts/launch.sh
@@ -16,11 +16,19 @@ fi
 
 cd "$(dirname "$0")"
 
+TARGET_KEYBOARD="${1:-keyball/keyball39custom}"
+TARGET_KEYBOARD_FAMILY="${TARGET_KEYBOARD%/*}"
+TARGET_KEYBOARD_KEYMAP="${2:-via}"
+
 main() {
 	CURRENT_DIR=$(pwd)
 	BASEFILE=$(readlink -f $0)
 	SCRIPT_DIR=$(dirname $BASEFILE)
-	QMK_KEYBOARDS_KEYBALL_DIR="$(cd ${SCRIPT_DIR%/}/../qmk_firmware/keyboards/keyball; pwd)"
+	QMK_KEYBOARDS_DIR="$(cd ${SCRIPT_DIR%/}/../qmk_firmware/keyboards/${TARGET_KEYBOARD_FAMILY}; pwd)"
+
+	echo "Keyboad: ${TARGET_KEYBOARD}"
+	echo "Keyboad Familly: ${TARGET_KEYBOARD_FAMILY}"
+	echo "Keyboad Keymap: ${TARGET_KEYBOARD_KEYMAP}"
 
 	cd $SCRIPT_DIR
 	. .env.local
@@ -31,10 +39,11 @@ main() {
 	cd $QMK_HOME
 	docker build . -t $DOCKER_CONTAINER_NAME
 
+	BUILD_TARGET=${TARGET_KEYBOARD}:${TARGET_KEYBOARD_KEYMAP}
 	echo "Launch: $BUILD_TARGET"
 	docker run -it --rm --tty \
 		-v $QMK_HOME:/qmk_firmware \
-		-v $QMK_KEYBOARDS_KEYBALL_DIR:/qmk_firmware/keyboards/keyball \
+		-v $QMK_KEYBOARDS_DIR:/qmk_firmware/keyboards/${TARGET_KEYBOARD_FAMILY}\
  	       $DOCKER_CONTAINER_NAME \
 		/bin/bash 
 }
